713ea8a3a0a715d4f6c541687a50aa4606f1e480
Pavel-Pokalnetov <redx@mail.ru>
2023-08-14 13:36:26 +0500

    ++

--- Accounting_of_pets/src/main/java/BaseClasses/Animals.java
+++ Accounting_of_pets/src/main/java/BaseClasses/Animals.java
@@ -4,29 +4,45 @@ import java.util.ArrayList;
 
 public class Animals {
     String name;
+    String type;
     String birthday;
-
     ArrayList<String> commandList = null;
 
     public Animals() {
 
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
     @Override
     public String toString() {
         StringBuilder result = new StringBuilder();
-        result.append("Имя: " + name + "\n");
-        result.append("Дата рождения: " + birthday + "\n");
+        result.append("Тип: ").append(type).append("\n");
+        result.append("Имя: ").append(name).append("\n");
+        result.append("Дата рождения: ").append(birthday).append("\n");
         result.append("Выполняемые команды:\n");
-        if ((commandList != null) && (commandList.size() > 0)) {
+        if ((commandList != null) && (!commandList.isEmpty())) {
             for (String item : commandList) {
-                result.append(" - "+item+"\n");
+                result.append(" - ").append(item).append("\n");
             }
-        }else {
+        } else {
             result.append("<командам не обучен>");
         }
         return result.toString();
     }
 
-
+    public String getJSON() {
+        
+        return  null;
+    }
 }
